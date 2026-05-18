1c6f34b2e35b3f29f0a11c4ec2267813b9441c87
nutsqwerty <acuzarhannah5@gmail.com>
2026-05-19 01:07:04 +0800

    Add files via upload

---
+++ js/thoughts.js
@@ -0,0 +1,7 @@
+const gentleThoughts = [
+    "Healing isn't a straight line, and having a heavy day doesn't erase all of your progress.",
+    "Take a deep breath. You don't have to figure everything out right this second.",
+    "You are doing the best you can with what you have, and that is completely enough.",
+    "Be gentle with yourself. You are growing, even if it feels invisible right now.",
+    "It is okay to rest. The world can wait while you take care of you."
+];
