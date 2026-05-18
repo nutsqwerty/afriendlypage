5d3b8fe0bf71b01e11ea6369a29a720dfb874328
nutsqwerty <acuzarhannah5@gmail.com>
2026-05-19 01:09:52 +0800

    Add modal functionality for displaying random thoughts

--- site.js
+++ site.js
@@ -1,4 +1,28 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+document.addEventListener("DOMContentLoaded", () => {
+    const jar = document.getElementById("jar-container"); 
+    const modal = document.getElementById("thought-modal");
+    const modalText = document.getElementById("modal-thought-text");
+    const nextLeafBtn = document.getElementById("btn-next-leaf");
+    const closeBoxBtn = document.getElementById("btn-close-box");
 
-// Write your JavaScript code.
+    if (jar) {
+        jar.addEventListener("click", () => {
+            showRandomThought();
+            if (modal) modal.style.display = "flex";
+        });
+    }
+
+    function showRandomThought() {
+        if (modalText && typeof gentleThoughts !== 'undefined' && gentleThoughts.length > 0) {
+            const randomIndex = Math.floor(Math.random() * gentleThoughts.length);
+            modalText.innerText = gentleThoughts[randomIndex];
+        }
+    }
+
+    if (nextLeafBtn) nextLeafBtn.addEventListener("click", showRandomThought);
+    if (closeBoxBtn) {
+        closeBoxBtn.addEventListener("click", () => {
+            if (modal) modal.style.display = "none";
+        });
+    }
+});
